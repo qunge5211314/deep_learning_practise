ebbc11b49590eb84514060ca6120289dc4b7ea47
JustinHan <>
2021-03-03 09:04:54 +0800

    no message

--- chapter2/task1.py
+++ chapter2/task1.py
@@ -15,7 +15,68 @@
 # Dependence
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from sklearn.metrics import accuracy_score
 
 
-def load_data():
-    pass
+# 加载原始数据
+def load_raw_data(path):
+    raw_data = pd.read_csv(path)
+    print("raw data:\n", raw_data.describe())
+    return raw_data
+
+
+# 数据清洗
+def clean_data(raw_data):
+    # 1.缺失值处理
+    cleaned_data1 = raw_data.dropna()
+    # 2.去重
+    cleaned_data = cleaned_data1.drop_duplicates()
+    print("cleaned data:\n", cleaned_data.describe())
+    return cleaned_data
+
+
+def show_cleaned_data(cleaned_data):
+    x = cleaned_data.drop(['y'], axis=1)
+    y = cleaned_data.loc[:, 'y']
+    fig1 = plt.figure(figsize=(5, 5))
+    label1 = plt.scatter(x.loc[:, 'x1'][y == 1], x.loc[:, 'x2'][y == 1])
+    label0 = plt.scatter(x.loc[:, 'x1'][y == 0], x.loc[:, 'x2'][y == 0])
+    plt.legend((label1, label0), ('label1', 'label0'))
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.title('raw data')
+    plt.show()
+
+
+if __name__ == '__main__':
+    path = "task1_data.csv"
+    # 获取原始数据
+    raw_data = load_raw_data(path)
+    # 数据清洗
+    cleaned_data = clean_data(raw_data)
+    # 数据可视化
+    # show_cleaned_data(cleaned_data)
+    # 数据分割
+    x_train, x_test, y_train, y_test = train_test_split(cleaned_data.drop(['y'], axis=1), cleaned_data.loc[:, "y"])
+    print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+    # 建立mlp模型
+    mlp_model = Sequential()
+    mlp_model.add(Dense(units=25, input_dim=2, activation='sigmoid'))
+    mlp_model.add(Dense(units=1, activation='sigmoid'))
+    print(mlp_model.summary())
+    # 模型求解参数配置
+    mlp_model.compile(optimizer='adam', loss='binary_crossentropy')
+    # 模型训练
+    mlp_model.fit(x_train, y_train, epochs=6000)
+    # 训练数据结果预测
+    y_train_predict = mlp_model.predict_classes(x_train)
+    # 表现评估
+    accuracy_train = accuracy_score(y_train, y_train_predict)
+    print(accuracy_train)
+    y_test_predict = mlp_model.predict_classes(x_test)
+    accuracy_test = accuracy_score(y_test, y_test_predict)
+    print(accuracy_test)
